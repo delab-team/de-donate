40a46200d06e5e8d843690cc3c4586601fb6e8f2
sijuz <sijuz@yandex.ru>
2023-10-07 18:06:35 +0300

    s

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 /* eslint-disable import/no-unresolved */
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import { v1 } from 'uuid'
 
 import { IconSelector, Input, Title } from '@delab-team/de-ui'
@@ -13,28 +13,65 @@ import IMG1 from '../../assets/img/01.png'
 import IMG2 from '../../assets/img/02.png'
 
 import s from './home.module.scss'
+import { Items, TonApi } from '../../logic/tonapi'
 
 interface HomePageProps {}
 
-const fundArray = [
+interface Fund {
+    img: string,
+    title: string,
+    amount: number,
+    target: number,
+    asset: string
+}
+
+const fundArray: Fund[] = [
     {
         img: IMG1,
         title: 'Tonstarter - xRocket',
         amount: 310,
-        target: 1000
+        target: 1000,
+        asset: 'TON'
     },
     {
         img: IMG2,
         title: 'Tonstarter Early Birds',
         amount: 1283,
-        target: 5000
+        target: 5000,
+        asset: 'TON'
     }
 ]
 
 export const HomePage: FC<HomePageProps> = () => {
     const [ value, setValue ] = useState<string>('')
+    const [ first, setFirst ] = useState<boolean>(false)
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)
+
+    useEffect(() => {
+        if (!first) {
+            setFirst(true)
+
+            const coll = 'kQBH_ElENh-if6t_j7Mr_NTPAzPnWJXOhJrXhumQhC__xxKu'
+
+            const api = new TonApi()
+
+            api.getItems(coll).then((items: Items | undefined) => {
+                console.log('api.getItems', items)
+                if (items) {
+                    for (let i = 0; i < items.nft_items.length; i++) {
+                        fundArray.push({
+                            title: '1',
+                            img: '1',
+                            amount: 1,
+                            target: 1,
+                            asset: 'TON'
+                        })
+                    }
+                }
+            })
+        }
+    }, [ ])
 
     return (
         <div className={s.home}>
