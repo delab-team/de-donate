36289374e8882d11afd2515bea1df5ac55044a31
anovic123 <anovicjosip@gmail.com>
2023-12-02 01:17:19 +0200

    commit

--- src/constants/jettons.tsx
+++ src/constants/jettons.tsx
@@ -12,23 +12,23 @@ export const jettons = [
         catalogs: null
     },
     {
+        address: [ '0:90605daea7f8494e554bb33a9ffb53849e958e0607985ec4ebcc77c6d952d401', '0:f6eb371de82aa9cfb5b22ca547f31fdc0fa0fbb41ae89ba84a73272ff0bf2157' ],
+        value: 'DFC',
+        label: 'DFC',
+        decimals: 9,
+        image: 'https://cache.tonapi.io/imgproxy/G8iWJLhU0kgZtKbA8j11QWRJYyCbq6E-Loq4TkMMMpA/rs:fill:200:200:1/g:no/aHR0cHM6Ly90YW4tdG91Z2gtc2x1Zy0zNTEubXlwaW5hdGEuY2xvdWQvaXBmcy9RbVhRb2pKVVB2a0dDQ2VSOVF1OFd3bWNaRjFnTERZMjhlcExMaFBZdkR5OFRr.webp',
+        description: 'DeFinder Capital',
+        social: null,
+        websites: null,
+        catalogs: null
+    },
+    {
         address: [ '0:729c13b6df2c07cbf0a06ab63d34af454f3d320ec1bcd8fb5c6d24d0806a17c2', '0:436b4ff60f7bacf75f70d422cfb6b5ae7cd2846f0f3553ae8bcc798258923608' ],
         value: 'jUSDT',
         label: 'jUSDT',
         decimals: 6,
         image: 'https://cache.tonapi.io/imgproxy/cUNZUfLE-OYozhOKWNw7HHINxER0pQnXhhtUkMQYWck/rs:fill:200:200:1/g:no/aHR0cHM6Ly9icmlkZ2UudG9uLm9yZy90b2tlbi8xLzB4ZGFjMTdmOTU4ZDJlZTUyM2EyMjA2MjA2OTk0NTk3YzEzZDgzMWVjNy5wbmc.webp',
         description: 'USDT transferred from Ethereum via bridge.ton.org. Token address in Ethereum: 0xdAC17F958D2ee523a2206206994597C13D831ec7',
-        social: null,
-        websites: null,
-        catalogs: null
-    },
-    {
-        address: [ '0:90605daea7f8494e554bb33a9ffb53849e958e0607985ec4ebcc77c6d952d401', '0:c288b9102a934e1a93435f6a194470da340559a95e9c9816c682ccff2215149d' ],
-        value: 'DLB',
-        label: 'DeLab',
-        decimals: 9,
-        image: 'https://cache.tonapi.io/imgproxy/yMTXtKd71h0hAgiw9gcIQDLl4QbNWslF7upPbCcZtc4/rs:fill:200:200:1/g:no/aHR0cHM6Ly9hdmF0YXJzLmdpdGh1YnVzZXJjb250ZW50LmNvbS91LzExNjg4NDc4OT9zPTIwMCZ2PTQ.webp',
-        description: 'DeLab Team Coin',
         social: null,
         websites: null,
         catalogs: null
